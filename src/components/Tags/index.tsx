b911ec93727ba32a471aff81146eadab527db61c
Pablo Rodríguez Caballero <pablo@rodriguezcaballero.me>
2021-09-23 17:41:28 +0200

    feat(Tags): handle overflowing tags

--- src/components/Tags/index.tsx
+++ src/components/Tags/index.tsx
@@ -1,14 +1,23 @@
+import { useTagOverflow } from './logic';
 import { Container } from './styles';
 import type { Props } from './types';
 
 import Tag from '$/components/Tag';
 
 export default function Tags({ children }: Props): JSX.Element {
+  const { anyHidden, containerRef, moreTagsLabel, moreTagsTitle, visibleTags } =
+    useTagOverflow({ children });
+
   return (
-    <Container>
-      {children.map((tag) => (
+    <Container ref={containerRef}>
+      {visibleTags.map((tag) => (
         <Tag key={tag}>{tag}</Tag>
       ))}
+      {anyHidden && (
+        <Tag key="more-tags" title={moreTagsTitle}>
+          {moreTagsLabel}
+        </Tag>
+      )}
     </Container>
   );
 }
